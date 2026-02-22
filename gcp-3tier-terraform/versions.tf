3983b8bb751f30b4c732e81a8f5de91486fdc5ae
Pujitha Potta <pujithapotta@Pujithas-MacBook-Air.local>
2026-02-22 17:59:48 +0530

    fix

---
+++ gcp-3tier-terraform/versions.tf
@@ -0,0 +1,14 @@
+
+terraform {
+	required_version = ">= 1.3.0, < 2.0.0"
+	required_providers {
+		google = {
+			source  = "hashicorp/google"
+			version = ">= 4.0.0, < 6.0.0"
+		}
+		random = {
+			source  = "hashicorp/random"
+			version = ">= 3.0.0, < 4.0.0"
+		}
+	}
+}
